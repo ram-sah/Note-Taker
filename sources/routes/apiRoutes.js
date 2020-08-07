84180b102e7cb0a17294b8f93ece5b45bb328727
Ram Sah <mohan2036@gmail.com>
2020-08-06 23:47:50 -0400

    added code to GET and POST notes

--- sources/routes/apiRoutes.js
+++ sources/routes/apiRoutes.js
@@ -1,12 +1,28 @@
 const fs = require("fs");
 var dbData = "./db/db.json";
-var parsedNotes = JSON.parse(fs.readFileSync(dbData, 'utf8'));
+// Read users.json file and store in parsedData
+var parsedData = JSON.parse(fs.readFileSync(dbData, 'utf8'));
 
-module.exports = function(app) {
-    app.get("/api/notes", (req, res) => {
-        return res.json(parsedNotes);
+// GET "/api/notes"  
+module.exports = function (route) {
+    route.get("/api/notes", (req, res) => {
+        // Converting to JSON 
+        return res.json(parsedData);
+    });
+    // POST "/api/notes"  
+    route.post("/api/notes", (req, res) => {
+        var newNote = req.body;
+        // create new id, make it 1 more than last id in array
+        newNote.id = (parsedData.length) + 1;
+        // push to array
+        parsedData.push(newNote);
+        // update file
+        fs.writeFileSync(dbData, JSON.stringify(parsedData));
+       return res.json(parsedData)
+    });
+    // DELETE "/api/notes" with an id
+    route.delete("/api/notes/:id", (req, res) => {
+
     });
 };
 
-
-  
