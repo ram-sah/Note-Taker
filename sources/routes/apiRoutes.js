809d013fcf51472181c240fdda36155280478149
Ram Sah <mohan2036@gmail.com>
2020-08-07 22:07:18 -0400

    fixed codes to make save function work

--- sources/routes/apiRoutes.js
+++ sources/routes/apiRoutes.js
@@ -1,9 +1,11 @@
+//Variables for path
 const fs = require("fs");
 var dbData = "./db/db.json";
-// Read users.json file and store in parsedData
+// Read dbData file and store in parsedData
 var parsedData = JSON.parse(fs.readFileSync(dbData, 'utf8'));
 
-// GET "/api/notes"  
+// GET "/api/notes"
+// module.exports allows this module to be used in other js files  
 module.exports = function (route) {
     route.get("/api/notes", (req, res) => {
         // Converting to JSON 
@@ -14,15 +16,16 @@ module.exports = function (route) {
         var newNote = req.body;
         // create new id, make it 1 more than last id in array
         newNote.id = (parsedData.length) + 1;
-        // push to array
+        // push newNote to array parseData
         parsedData.push(newNote);
         // update file
         fs.writeFileSync(dbData, JSON.stringify(parsedData));
        return res.json(parsedData)
     });
-    // DELETE "/api/notes" with an id
-    route.delete("/api/notes/:id", (req, res) => {
+    // // DELETE "/api/notes" with an id
+    // route.delete("/api/notes/:id", (req, res) => {
 
-    });
+    // });
+    
 };
 
