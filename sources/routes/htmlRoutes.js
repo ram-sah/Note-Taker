642f1367279007d5f3f91358d3bbdae353f3b30c
Ram Sah <mohan2036@gmail.com>
2020-08-07 22:09:48 -0400

    codes explain

--- sources/routes/htmlRoutes.js
+++ sources/routes/htmlRoutes.js
@@ -1,12 +1,13 @@
-
+// these html routes help to controls, what page is delivered to the user. 
 var path = require("path");
+// module.exports allows this module to be used in other js files. 
 module.exports = function (route) {
   //Page 2 - set notes.html file
   route.get("/notes", function (req, res) {
     console.log("route");
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
-  //Page 1 - set index.html file
+   // GET "*" - Will return the "/index.html" file
   route.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
